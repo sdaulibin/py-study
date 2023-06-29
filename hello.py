fa05f22809b064cc54103e951616c8cacfc8a792
binginx <sdaulibin@126.com>
2023-06-29 17:40:35 +0800

    python study

---
+++ hello.py
@@ -0,0 +1,14 @@
+print("hello world")
+print(3*90)
+
+# name = input("请输入你的名字:")
+# print(name)
+
+print("hello\tworld")
+print("hello    world")
+
+import keyword
+print(keyword.kwlist)
+
+if 2>1:
+    print("hello")
